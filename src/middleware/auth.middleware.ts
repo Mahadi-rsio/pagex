c3d14a506017fd3b134993a6e694b9d4153cecfd
Mahadi-rsio <rsioex@gmail.com>
2026-04-17 09:16:17 +0600

    fix: Fixed types at auth middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -13,8 +13,8 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
     const token = authHeader.split(' ')[1]
 
     try {
-        const { payload } = await jwtVerify(token, JWKS)
-        req.user = payload
+        const { payload } = await jwtVerify(token!, JWKS)
+            ; (req as any).user = payload
         next()
     } catch (err) {
         return res.status(401).json({ error: 'Invalid or expired token' })
