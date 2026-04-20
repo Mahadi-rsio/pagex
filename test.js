6d59392713c196d160289e3209de60c5347a1c96
Mahibur Rahman Mahade <rsioex@gmail.com>
2026-04-20 09:06:33 +0000

    Updated test.js for better test for the api

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = "http://api.localhost/api";
 
 // ⚠️ Put your token here manually
 const TOKEN = "eyJhbGciOiJFZERTQSIsImtpZCI6IjU2OGZlN2I2LTg4NmItNDlmNS05YmYxLWEyZTgwNmM2MWExNyJ9.eyJpYXQiOjE3NzYzOTU5NTYsIm5hbWUiOiJSc2lvIEV4IiwiZW1haWwiOiJyc2lvZXhAZ21haWwuY29tIiwiZW1haWxWZXJpZmllZCI6dHJ1ZSwiaW1hZ2UiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NJdWJpRDhLZ3hGN2tDeWRYMDVvMWZ2eWFxSXpkY183Vi1ubEM4b2tIaW9IbkthM1E9czk2LWMiLCJjcmVhdGVkQXQiOiIyMDI2LTA0LTE2VDIyOjQ0OjA2Ljk0MFoiLCJ1cGRhdGVkQXQiOiIyMDI2LTA0LTE2VDIyOjQ0OjA2Ljk0MFoiLCJpZCI6Ilk2RDJuZUZHRkpxbTdVenJpakVNa1RZclBZbXRwdW5iIiwic3ViIjoiWTZEMm5lRkdGSnFtN1V6cmlqRU1rVFlyUFltdHB1bmIiLCJleHAiOjE3NzcwMDA3NTYsImlzcyI6Imh0dHBzOi8vY2xvdWRpc3kudmVyY2VsLmFwcCIsImF1ZCI6Imh0dHBzOi8vY2xvdWRpc3kudmVyY2VsLmFwcCJ9.7izpUl0UC4lRa8hsQQI0I1GRkcGC-gD-aTlvzHQ41F2-YOu88mBp_O8EnEnXNdYfqPbD_rbQx3MUD-PEOaT4DA";
@@ -21,7 +21,7 @@ async function apiRequest(endpoint, { method = "GET", body } = {}) {
     }
 
 
-    return data
+    return data.json()
 }
 
 /**
@@ -38,14 +38,23 @@ async function createPage(payload) {
  * Get usage by domain
  */
 async function getUsage(domain) {
-    await apiRequest(`/pages/usage/${domain}`, {
+    const data = await apiRequest(`/pages/usage/${domain}`, {
         method: "GET",
     });
+    console.log(data)
 }
 
 
 async function m() {
-    await getUsage("cloud.localhost")
+    
+	/**await createPage({
+	    tenant_name : "mahadi",
+	    plan : "free",
+	    project_name: "hello"
+    
+	}) **/
+    await getUsage("hello.localhost")
+
 }
 
 m()
