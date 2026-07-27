cb49772198780d87bde4eca22815c4b124ac0a22
Mahibur Rahman Mahade <rsioex@gmail.com>
2026-07-27 17:13:32 +0000

    fixing issue at this project

--- services/api/drizzle.config.ts
+++ services/api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 import 'dotenv/config'
 
 export default defineConfig({
-    schema: "./src/infrastructure/db/schema.ts",
+    schema: "./infrastructure/db/schema.ts",
     dialect: "postgresql",
     out: "./drizzle",
     dbCredentials: {
